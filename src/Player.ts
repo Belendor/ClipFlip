5a0d089ab705a340f9d4ba0b910cecf1217b52a8
Belendor <dwarf11@gmail.com>
2025-08-16 12:29:55 +0300

    update

--- src/Player.ts
+++ src/Player.ts
@@ -15,7 +15,7 @@ class Players {
     state: State;
     html: HTML;
     active: Record<number, boolean> | undefined;
-    folder = '/video/';
+    folder = 'https://www.clipflip.online/video/';
     muted: boolean = true;
     playerCount: number = 8;
     selectedTags: Map<number, string> = new Map();
