e0cca1c8ab0b2dfa303c9eaffa756f805093bd34
Belendor <dwarf11@gmail.com>
2026-01-21 04:23:46 +0200

    update

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ console.log(`Running in ${isProduction ? 'production' : 'development'} mode`);
 export const config = {
     // If Linux (EC2), use the production domain. If Windows, use localhost.
     baseUrl: isProduction ? 'https://clip-flip.com' : 'http://localhost:3000',
-    videoSourcePath: isProduction ? './video/' : 'https://clip-flip.com/video/',
+    videoSourcePath: isProduction ? '/video/' : 'https://clip-flip.com/video/',
     defaultPercentChance: 25,
     defaultEndIndex: 5609,
     apiUrl: isProduction ? 'https://clip-flip.com/api' : 'https://clip-flip.com/api',
